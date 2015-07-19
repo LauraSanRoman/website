5746e6b56bea978680ca252da5769f42553cb738
LauraSanRoman <los5300@psu.edu>
2015-07-19 17:39:38 -0400

    Skeleton and color scheme

---
+++ js/recent_projects.js
@@ -0,0 +1,8 @@
+(function  myDate(){
+  var today_date= new Date()
+   var myyear=today_date.getYear()
+   var mymonth=today_date.getMonth()+1
+   var mytoday=today_date.getDate()
+   document.write(myyear+"/"+mymonth+"/"+mytoday)
+
+});
